ae742d9d95714c2490ec3dd8eafae569f49e66d8
adithkrishnaa <sak162000@gmail.com>
2024-02-26 10:07:01 +0530

    added

---
+++ javascript Fundementals/script.js
@@ -0,0 +1,94 @@
+
+
+// //one line commend
+
+// /*multi
+// level 
+// commend*/
+
+
+// // let user = 'john',age=23,message="hello";
+
+
+// // let name = "Ilya";
+
+// // alert( `hello ${1}` ); 
+
+// // alert( `hello ${"name"}` ); 
+
+// // alert( `hello ${name}` );
+
+
+
+// let message = (login == "employee") ? "hello" : (login == "director") ? "Greeting"
+// : (login == " ") ? "No login" : " ";
+
+
+
+
+
+// let username = prompt("enter your name"," ");
+ 
+// if (username === "Admin"){
+//     let password = prompt("Enter your Password"," ");
+//     if (password === "Good"){
+//         alert("welcome");
+//     }
+//     else if (password === " " || password === null) {
+//             alert("Enter a Value");
+//         }
+//         else{
+//             alert("Wrong Password");
+//         }
+// }
+// else if (username===" " || username === null){
+//     alert("Cancled")
+// }
+// else {
+// alert("Your are not a user");
+// }
+
+
+// let username = "chandre";
+
+
+// function myName(){
+
+//    let message ="Hi am" +username ;
+//     alert (message);
+// }
+
+// function ourName() {
+    
+//    let ourmessage = "Hi am" + username ;
+//     alert (ourmessage);
+// }
+
+// myName();
+
+// ourName();
+
+
+
+// function ask(question,yes,no){
+//     if(confirm(question))yes();
+//     else no();
+// }
+
+// ask(
+//     "do you agree?",
+//  () => alert("You agree"),
+//   () => alert("You excuation is canclled"));
+
+
+// let accessAllowed;
+// function accessAllowed(){
+//     let age = prompt('How old are you?', '');
+//     return age>18 ? accessAllowed=true : accessAllowed=false ;
+// }
+// alert(accessAllowed);
+
+
+
+
+
